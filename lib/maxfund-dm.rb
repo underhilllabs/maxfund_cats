818dd08322b6919375503b37498fb0f683c4e8bd
Bart Lantz <bartlantz@gmail.com>
2015-09-28 09:41:30 -0600

    Fixes DataMapper SQLite error with connection string

--- lib/maxfund-dm.rb
+++ lib/maxfund-dm.rb
@@ -1,6 +1,8 @@
-DataMapper.setup(:default, 'sqlite:///maxfund_cats.db')
+require 'data_mapper'
+DataMapper.setup(:default, 'sqlite:data/maxfund_cats.db')
+puts "current dir is #{Dir.pwd}"
 
-class Post
+class CatDM
   include DataMapper::Resource
 
   property :id,         Serial    # An auto-increment integer key
@@ -14,6 +16,7 @@ class Post
   property :image_url,      String
   property :description,       Text      # A text block, for longer string data.
   property :created_at, DateTime  # A DateTime, for any date you might like.
-endD
+end
 
-DataMapper.finalize
+#DataMapper.finalize
+DataMapper.auto_upgrade!
