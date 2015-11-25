8642905f1e2273c584d8f2293b1a7af671519990
Bart Lantz <underhilllabs@gmail.com>
2015-11-25 02:56:33 +0000

    changes default page to be angular app

--- app.rb
+++ app.rb
@@ -3,11 +3,14 @@ require 'sinatra'
 require './lib/maxfund-dm'
 
 get '/' do
+  erb :cats
+end
+
+get '/cats' do
   @cats = CatDM.current
   @cat_alumni = CatDM.alumni
   haml :welcome
 end
-
 
 get '/cat/:id' do
   @cat = CatDM.get(params[:id])
@@ -17,9 +20,5 @@ end
 get '/cats.json' do
   content_type :json
   @cats = CatDM.all.to_json
-end
-
-get '/cats' do
-  erb :cats
 end
 
