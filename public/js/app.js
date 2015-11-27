1fa0801cb321d829854b7ab9729e55c40d46c9eb
Bart Lantz <underhilllabs@gmail.com>
2015-11-27 01:48:04 +0000

    Removes some commented out text for readability

--- public/js/app.js
+++ public/js/app.js
@@ -32,35 +32,27 @@ app.controller("mainController", function($scope, $http, animalModel) {
   $scope.current = [];
   $scope.alum = [];
   $scope.filterText = null;
-  //$scope.availableCategories = [];
   $scope.availableLocs = [];
-  //$scope.categoryFilter = null;
   $scope.filterSpec = [];
   $scope.filters = {};
-  // $scope.setCategoryFilter = function(category) {
-  //   $scope.categoryFilter = category;
-  //};
   results = animalModel.init();
   $scope.current = results[0];
   $scope.alum = results[1];
-  //$scope.availableCategories = animalModel.getCategories();
+
   $scope.availableLocs = animalModel.getLocs();
 });
 app.factory('animalModel', function($http) {
   var classes = [];
-  //var availableCategories = [];
   var availableLocs = [];
   var results = [];
   var alum = [];
   var current = [];
 
   var init = function() {
-    // Download the spreadsheet data and add it to the scope objects above
     $http({
         url: '/cats.json',
         method: "GET"
     }).success(function(data) {
-
       angular.forEach(data, function(value, index) {
           if(value.is_current == 0) {
               alum.push(value);
@@ -70,32 +62,6 @@ app.factory('animalModel', function($http) {
           if(availableLocs.indexOf(value.loc) == -1) {
               availableLocs.push(value.loc);
           }
-
-          //console.log("cat: " + value.image);
-          // Building personality array
-          // angular.forEach(classes.gsx$personality, function(category, index) {
-          //     var exists = false;
-          //     angular.forEach(availableCategories, function(avCat, index) {
-          //         if (avCat == category) {
-          //             exists = true;
-          //         }
-          //     });
-          //     if (exists === false) {
-          //         availableCategories.push(category);
-          //     }
-          // });
-          // // Building species array
-          // angular.forEach(classes.gsx$species, function(species, index) {
-          //     var exists = false;
-          //     angular.forEach(availableSpecies, function(avSpec, index) {
-          //         if (avSpec == species) {
-          //             exists = true;
-          //         }
-          //     });
-          //     if (exists === false) {
-          //         availableSpecies.push(species);
-          //     }
-          // });
       });
     }).error(function(error) {
         Console.log("Err:" + error);
@@ -108,6 +74,5 @@ app.factory('animalModel', function($http) {
   return {
     init: init,
     getLocs: getLocs,
-    //getCategories: getCategories
-  };
+   };
 });
